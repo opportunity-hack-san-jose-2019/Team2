046fb6904b85699d20b70f830dc70987034d93c5
AppleTesla808 <jkschrei@gmail.com>
2019-11-02 15:18:12 -0700

    base project

---
+++ YearUp Connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YearUp Connect
+//
+//  Created by Jaron Schreiber on 11/2/19.
+//  Copyright © 2019 no_more_disk_space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
