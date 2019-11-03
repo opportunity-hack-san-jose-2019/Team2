f27301a4f0e016553c76bf26716895bbb21a9aae
piercetu <piercetu@gmail.com>
2019-11-03 00:38:06 -0700

    3 dots

---
+++ OppHack19T2/OppHack19T2/Controller/pickerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  pickerViewController.swift
+//  OppHack19T2
+//
+//  Created by Pierce Tu on 11/2/19.
+//  Copyright © 2019 Pierce Tu. All rights reserved.
+//
+
+import UIKit
+
+class pickerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- OppHack19T2/OppHack19T2/Controller/textFieldViewController.swift
+++ OppHack19T2/OppHack19T2/Controller/textFieldViewController.swift
@@ -1,18 +1,14 @@
 //
-//  PickerViewController.swift
+//  textFieldViewController.swift
 //  OppHack19T2
 //
-//  Created by Pierce Tu on 11/2/19.
+//  Created by Pierce Tu on 11/3/19.
 //  Copyright © 2019 Pierce Tu. All rights reserved.
 //
 
 import UIKit
 
-class PickerViewController: UIViewController {
-    
-    
-    @IBOutlet var test: UIView!
-    
+class textFieldViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
